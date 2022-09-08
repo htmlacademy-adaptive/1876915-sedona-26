1a01073efe1a6a8bfc71a944e590a1f4a8998b09
Keks <keks@htmlacademy.ru>
2022-09-08 15:42:43 +0300

    :heavy_check_mark: Сборка #5

--- 5/js/main.js
+++ 5/js/main.js
@@ -1,16 +1,16 @@
 document.querySelector('.main-header--no-js')?.classList.remove('main-header--no-js');
 const navButton = document.querySelector('.main-nav__button');
-const navMenu = document.querySelector('.main-nav__menu');
+const navMenu = document.querySelector('.main-nav__list');
 
 const onNavButtonClick = () => {
   if (navButton.classList.contains('main-nav__button--opened')) {
     navButton.classList.remove('main-nav__button--opened');
     navButton.classList.add('main-nav__button--closed');
-    navMenu.classList.add('main-nav__menu--hidden');
+    navMenu.classList.add('main-nav__list--hidden');
   } else {
     navButton.classList.add('main-nav__button--opened');
     navButton.classList.remove('main-nav__button--closed');
-    navMenu.classList.remove('main-nav__menu--hidden');
+    navMenu.classList.remove('main-nav__list--hidden');
   }
 }
 
